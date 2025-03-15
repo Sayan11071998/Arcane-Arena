7b587a0bb786ebcfe015e4f804c7ece7d7c8ae52
Sayan Nandi <sayannandi11071998@gmail.com>
2025-03-16 00:48:18 +0530

    Concrete Commands Created

--- Assets/Scripts/Commands/Abstract Commands/UnitCommand.cs
+++ Assets/Scripts/Commands/Abstract Commands/UnitCommand.cs
@@ -6,16 +6,15 @@ namespace Command.Commands
 {
     public abstract class UnitCommand : ICommand
     {
-        public int actorUnitID;
-        public int TargetUnitID;
-        public int ActorPlayerID;
-        public int TargetPlayerID;
+        public CommandData commandData;
 
         protected UnitController actorUnit;
         protected UnitController targetUnit;
 
         public abstract void Execute();
-
         public abstract bool WillHitTarget();
+
+        public void SetActorUnit(UnitController actorUnit) => this.actorUnit = actorUnit;
+        public void SetTargetUnit(UnitController targetrUnit) => this.targetUnit = targetrUnit;
     }
 }
