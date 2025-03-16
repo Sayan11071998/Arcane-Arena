4bcc15aebd351694870a767a26f71fe15d6f6255
Sayan Nandi <sayannandi11071998@gmail.com>
2025-03-17 00:21:19 +0530

    Command Invoker Updated

--- Assets/Scripts/Commands/CommandInvoker.cs
+++ Assets/Scripts/Commands/CommandInvoker.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using Command.Main;
 
 namespace Command.Commands
 {
@@ -14,5 +15,14 @@ namespace Command.Commands
 
         public void ExecuteCommand(ICommand commandToExecute) => commandToExecute.Execute();
         public void RegisterCommand(ICommand commandToRegister) => commandRegistry.Push(commandToRegister);
+
+        public void Undo()
+        {
+            if (!RegistryEmpty() && CommandBelongsToActivePlayer())
+                commandRegistry.Pop().Undo();
+        }
+
+        private bool RegistryEmpty() => commandRegistry.Count == 0;
+        private bool CommandBelongsToActivePlayer() => (commandRegistry.Peek() as UnitCommand).commandData.ActorPlayerID == GameService.Instance.PlayerService.ActivePlayerID;
     }
 }
